f3f10811429e1a96685502c6ce1f1d39a3f2407e
seunkim <seunkim@ea.com>
2020-07-03 23:46:03 +0900

    Update initial project

---
+++ js/demo/chart-memory.js
@@ -0,0 +1,158 @@
+// Area Chart Example
+var ctx = document.getElementById("memory").getContext('2d');
+var cpuChart = new Chart(ctx, {
+  data: {
+    datasets: [{
+      type: 'line',
+      label: "auth",
+      lineTension: 0.5,
+      backgroundColor: color(chartColors[5]).alpha(0.1).rgbString(),
+      borderColor: chartColors[5],
+      borderWidth: 0.5,
+      borderCapStyle: 'round',
+      pointHoverBorderWidth: 2,
+      pointRadius: 0,
+      data: generateData()
+    }], 
+  },
+  options: {
+    animation: {
+      duration: 0
+    },
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        distribution: 'series',
+        offset: true,
+        ticks: {
+          source: 'data',
+          autoSkip: true,
+          autoSkipPadding: 75,
+          maxRotation: 0,
+          sampleSize: 100
+        },
+      }],
+      yAxes: [{
+      }],
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      align: 'start',
+      labels: {
+          boxWidth: 10,
+          fontSize: 10,
+          fontColor: 'rgb(255, 99, 132)'
+      }
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 10,
+      yPadding: 10,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    }
+  }
+});
+
+ctx = document.getElementById("memory_max").getContext('2d');
+var cpuMaxChart = new Chart(ctx, {
+  data: {
+    datasets: [{
+      type: 'line',
+      label: "auth",
+      lineTension: 0.5,
+      backgroundColor: color(chartColors[5]).alpha(0.1).rgbString(),
+      borderColor: chartColors[5],
+      borderWidth: 0.5,
+      borderCapStyle: 'round',
+      pointHoverBorderWidth: 2,
+      pointRadius: 0,
+      data: generateData()
+    }], 
+  },
+  options: {
+    animation: {
+      duration: 0
+    },
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        type: 'time',
+        distribution: 'series',
+        offset: true,
+        ticks: {
+          source: 'data',
+          autoSkip: true,
+          autoSkipPadding: 75,
+          maxRotation: 0,
+          sampleSize: 100
+        },
+      }],
+      yAxes: [{
+      }],
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      align: 'start',
+      labels: {
+          boxWidth: 10,
+          fontSize: 10,
+          fontColor: 'rgb(255, 99, 132)'
+      }
+    },
+    tooltips: {
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 14,
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 10,
+      yPadding: 10,
+      displayColors: false,
+      intersect: false,
+      mode: 'index',
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+        }
+      }
+    }
+  }
+});
---
+++ js/demo/utils.js
@@ -0,0 +1,137 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+function number_format(number, decimals, dec_point, thousands_sep) {
+  // *     example: number_format(1234.56, 2, ',', ' ');
+  // *     return: '1 234,56'
+  number = (number + '').replace(',', '').replace(' ', '');
+  var n = !isFinite(+number) ? 0 : +number,
+    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+    s = '',
+    toFixedFix = function(n, prec) {
+      var k = Math.pow(10, prec);
+      return '' + Math.round(n * k) / k;
+    };
+  // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+  s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+  if (s[0].length > 3) {
+    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+  }
+  if ((s[1] || '').length < prec) {
+    s[1] = s[1] || '';
+    s[1] += new Array(prec - s[1].length + 1).join('0');
+  }
+  return s.join(dec);
+}
+
+function generateData() {
+  var unit = 'hour';
+
+  function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  function randomBar(date, lastClose) {
+    var open = randomNumber(lastClose * 0.95, lastClose * 1.05).toFixed(2);
+    var close = randomNumber(open * 0.95, open * 1.05).toFixed(2);
+    return {
+      t: date.valueOf(),
+      y: close
+    };
+  }
+
+  var date = moment('Jan 01 1990', 'MMM DD YYYY');
+  var now = moment();
+  var data = [];
+  for (; data.length < 600 && date.isBefore(now); date = date.clone().add(1, unit).startOf(unit)) {
+    data.push(randomBar(date, data.length > 0 ? data[data.length - 1].y : 30));
+  }
+  console.log(data);
+  return data;
+}
+
+var original = Chart.defaults.global.legend.onClick;
+Chart.defaults.global.legend.onClick = function(e, legendItem) {
+  var index = legendItem.datasetIndex;
+  var ci = this.chart;
+  var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;       
+  var anyOthersAlreadyHidden = false;
+  var allOthersHidden = true;
+
+  // figure out the current state of the labels
+  ci.data.datasets.forEach(function(e, i) {
+    var meta = ci.getDatasetMeta(i);
+
+    if (i !== index) {
+      if (meta.hidden) {
+        anyOthersAlreadyHidden = true;
+      } else {
+        allOthersHidden = false;
+      }
+    }
+  });
+
+  // if the label we clicked is already hidden 
+  // then we now want to unhide (with any others already unhidden)
+  if (alreadyHidden) {
+    ci.getDatasetMeta(index).hidden = null;
+  } else { 
+    // otherwise, lets figure out how to toggle visibility based upon the current state
+    ci.data.datasets.forEach(function(e, i) {
+      var meta = ci.getDatasetMeta(i);
+
+      if (i !== index) {
+        // handles logic when we click on visible hidden label and there is currently at least
+        // one other label that is visible and at least one other label already hidden
+        // (we want to keep those already hidden still hidden)
+        if (anyOthersAlreadyHidden && !allOthersHidden) {
+          meta.hidden = true;
+        } else {
+          // toggle visibility
+          meta.hidden = meta.hidden === null ? !meta.hidden : null;
+        }
+      } else {
+        meta.hidden = null;
+      }
+    });
+  }
+
+  ci.update();
+}
+
+/* Create color array */
+/* https://codenebula.io/javascript/frontend/dataviz/2019/04/18/automatically-generate-chart-colors-with-chart-js-d3s-color-scales/ */
+var color = Chart.helpers.color;
+const colorScale = d3.interpolateInferno;
+const colorRangeInfo = {
+  colorStart: 0,
+  colorEnd: 1,
+  useEndAsStart: true,
+}
+function interpolateColors(dataLength, colorScale, colorRangeInfo) {
+  var { colorStart, colorEnd } = colorRangeInfo;
+  var colorRange = colorEnd - colorStart;
+  var intervalSize = colorRange / dataLength;
+  var i, colorPoint;
+  var colorArray = [];
+
+  for (i = 0; i < dataLength; i++) {
+    colorPoint = calculatePoint(i, intervalSize, colorRangeInfo);
+    colorArray.push(colorScale(colorPoint));
+  }
+
+  return colorArray;
+}
+
+function calculatePoint(i, intervalSize, colorRangeInfo) {
+  var { colorStart, colorEnd, useEndAsStart } = colorRangeInfo;
+  return (useEndAsStart
+    ? (colorEnd - (i * intervalSize))
+    : (colorStart + (i * intervalSize)));
+}
+
+var chartColors = interpolateColors(10, colorScale, colorRangeInfo);
+
