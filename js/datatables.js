9812236ed2df9becafe274c451cdc4ffefba6268
seunkim <seunkim@ea.com>
2020-07-31 12:06:56 +0900

    Improved server metric table

--- js/datatables.js
+++ js/datatables.js
@@ -48,10 +48,43 @@ $(document).ready(function() {
 $(document).ready(function() {
   $('#serverMetric').DataTable({
     pageLength: 100,
+    'rowCallback': function(row, data, index) {
+      console.log(data.cpu)
+      console.log(data.memory)
+      var reg = /([0-9.]*)% \/ ([0-9.]*)% \/ ([0-9.]*)%/i;
+      var cpuMatchs = data.cpu.match(reg);
+      var memMatchs = data.memory.match(reg);
+
+      var isCpuOverTarget = false;
+      var isMemOverTarget = false;
+
+      var cpuCriteria = parseFloat(data.cpuCriteria.replace('%', ''));
+      var memCriteria = parseFloat(data.memCriteria.replace('%', ''));
+
+      for (i=1; i < 4; i++) {
+        if (parseFloat(cpuMatchs[i]) >= cpuCriteria) {
+          isCpuOverTarget = true;
+        }
+
+        if (parseFloat(memMatchs[i]) >= memCriteria) {
+          isMemOverTarget = true;
+        }
+      }
+
+      if (isCpuOverTarget) {
+        $('td:eq(2)', row).css('color', 'red');
+      }
+
+      if (isMemOverTarget) {
+        $('td:eq(4)', row).css('color', 'red');
+      }
+    },
     data: serverMetric,
     columns: [
       { data: 'serviceName' },
+      { data: 'cpuCriteria' },
       { data: 'cpu' },
+      { data: 'memCriteria' },
       { data: 'memory' }
     ]
   });
